b79eb23e1dc21d878462caa89ff91e299d05386c
Pratham Paleriya <prathampaleriya@gmail.com>
2025-12-25 17:36:39 +0530

    fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -128,7 +128,7 @@ const Hero = () => {
           <a href="https://wa.me/message/A5232GUJTZAGK1" target='_blank' className="bg-white hover:bg-white/100 text-[#726DC5] px-14 py-3 rounded-full text-sm font-semibold shadow-lg hover:shadow-xl transition-all duration-300 cursor-pointer min-w-[160px]">
             Talk to Eira
           </a>
-          <div className='text-sm mt-2 text-white'>( currently in beta )</div>
+          <div className='text-sm mt-2 text-white mt-4'>( currently in beta )</div>
         </motion.div>
 
         <motion.div
